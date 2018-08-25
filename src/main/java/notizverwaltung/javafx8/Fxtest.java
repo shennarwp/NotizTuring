de60b0c1f8d64dd76be2af2fe2a82d3a63e8963a
miblau <michelle.blau@gmx.net>
2018-08-25 12:15:06 +0200

    erstelleNotizDialog() hinzugefügt

--- src/main/java/notizverwaltung/javafx8/Fxtest.java
+++ src/main/java/notizverwaltung/javafx8/Fxtest.java
@@ -1,18 +1,30 @@
 package notizverwaltung.javafx8;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.input.DragEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import notizverwaltung.i18n.I18nMenuUtil;
+import notizverwaltung.model.interfaces.*;
+
+import java.awt.event.MouseEvent;
 
 import static javafx.application.Application.launch;
 
+
+/**
+ * Stellt das Grundfenster ohne weitere Funktionalität zur Verfügung
+ *
+ *
+ * @author Michelle Blau
+ * @version 25.08.2018
+ */
 public class Fxtest extends Application {
 
     @Override
@@ -28,7 +40,7 @@ public class Fxtest extends Application {
 
         Scene scene = new Scene(root, 250, 75);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Notizverwaltung");
+        primaryStage.setTitle("Notizverwaltung"); //TODO: i18n
         primaryStage.setResizable(true);
         primaryStage.centerOnScreen();
         primaryStage.setMaximized(true);
@@ -46,7 +58,19 @@ public class Fxtest extends Application {
         final Menu menuDatei = new Menu(I18nMenuUtil.getMenuFile());
         final MenuItem itmNeu = new MenuItem(I18nMenuUtil.getMenuNew());
 
+        //TODO: i18n
         final Menu menuNeu = new Menu(I18nMenuUtil.getMenuNew());
+        final MenuItem itmKategorie = new MenuItem("Kategorie");
+        final MenuItem itmBearbeitungszustand = new MenuItem("Bearbeitungszustand");
+        final MenuItem itmNotiz = new MenuItem("Notiz");
+
+        EventHandler<ActionEvent> lambdaErstelleNotizDialog = (actionevent) -> {
+           erstelleNotizDialog().showAndWait();
+        };
+
+
+        //itmNotiz.addEventHandler(DragEvent. , lambdaErstelleNotizDialog);
+
         final Menu menuBearbeiten = new Menu(I18nMenuUtil.getMenuEdit());
         final Menu menuOptionen = new Menu(I18nMenuUtil.getMenuOptions());
         final Menu menuHilfe = new Menu(I18nMenuUtil.getMenuHelp());
@@ -56,12 +80,47 @@ public class Fxtest extends Application {
 
 
         menuDatei.getItems().addAll(itmNeu);
+        menuNeu.getItems().addAll(itmKategorie,itmBearbeitungszustand,itmNotiz);
 
         menuBar.getMenus().addAll(menuDatei, menuNeu, menuBearbeiten, menuOptionen, menuHilfe, menuSuche, menuFilter, menuSortieren);
 
         return menuBar;
     }
 
+
+
+
+
+    private Dialog erstelleNotizDialog(){
+
+        Dialog<Notiz> erstelleNotizDialog = new Dialog<>();
+
+        erstelleNotizDialog.setTitle("Neue Notiz erstellen");
+        erstelleNotizDialog.setHeaderText("This is a custom dialog. Enter info and \n" +
+                "press Okay (or click title bar 'X' for cancel).");
+        erstelleNotizDialog.setResizable(true);
+
+        Label label1 = new Label("Name: ");
+        Label label2 = new Label("Phone: ");
+        TextField text1 = new TextField();
+        TextField text2 = new TextField();
+
+        GridPane grid = new GridPane();
+        grid.add(label1, 1, 1);
+        grid.add(text1, 2, 1);
+        grid.add(label2, 1, 2);
+        grid.add(text2, 2, 2);
+
+        erstelleNotizDialog.getDialogPane().setContent(grid);
+
+
+
+        ButtonType buttonTypeOk = new ButtonType("Okay", ButtonBar.ButtonData.OK_DONE);
+
+        erstelleNotizDialog.getDialogPane().getButtonTypes().add(buttonTypeOk);
+
+        return erstelleNotizDialog;
+    }
 
 }
 
