00f43d53cb5a0c25358ce1b70bd4641e4a8d5be9
TG <orggottschalk@web.de>
2018-09-13 15:39:45 +0200

    Testklassen gelöscht

--- src/main/Java/notizverwaltung/service/interfaces/NotizFilterService.java
+++ src/main/Java/notizverwaltung/service/interfaces/NotizFilterService.java
@@ -5,12 +5,20 @@ import notizverwaltung.model.interfaces.Notiz;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ *
+ * Die Klasse stellt Funktionalitäten für Notizen bereit
+ *
+ *
+ * @author Tobias Gottschalk
+ * @version 1.0
+ * @since
+ */
 public interface NotizFilterService {
 
     public List<Notiz> filterAlleNotizenMitLambda (Predicate<Notiz> notizPredicate, List<Notiz> listeVonNotiz);
 
-    public List<Notiz> filterAlleNotizenMitPriorität(int notizblockID, List<Notiz> listVonNotizen);
-    public List<Notiz> filterAlleNotizenOhnePriorität (int notizblockID, List<Notiz> listVonNotizen);
-
-    public List<Notiz> filterAlleNotizenMitEinemBearbeitunszustand(int bearbeitungszustandID, List<Notiz> listVonNotizen);
+    public List<Notiz> filterAlleNotizenMitPriorität(int notizblockID,int bearbeitungszustandsID);
+    public List<Notiz> filterAlleNotizenOhnePriorität (int notizblockID, int bearbeitungszustandsID);
+    public List<Notiz> filterAlleNotizenMitEinemBearbeitunszustand(int notizblockID,int bearbeitungszustandID);
 }
