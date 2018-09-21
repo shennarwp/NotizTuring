93e82bb557b8bda8668b8272abddac0f87ecb130
miblau <michelle.blau@gmx.net>
2018-09-21 15:43:38 +0200

    OverviewController verwenden FXListen (Properties+DataBinding) Kategorie-Namen-Änderungen werden noch nicht in GUI aktualisiert equals()-Methoden für BearbeitungszustandFX,KategorieFX implementiert + getestet

--- src/test/java/notizverwaltung/model/classes/NotizFXImplTest.java
+++ src/test/java/notizverwaltung/model/classes/NotizFXImplTest.java
@@ -3,6 +3,7 @@ package notizverwaltung.model.classes;
 import notizverwaltung.exceptions.IntIstNegativException;
 import notizverwaltung.exceptions.ObjectIstNullException;
 import notizverwaltung.exceptions.StringIsEmptyException;
+import notizverwaltung.model.interfaces.NotizFX;
 import notizverwaltung.validators.ObjectValidator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Testklasse zum Testen der Klasse NotizFXImpl
  * @author Kevin Engelhardt
+ * @author Michelle Blau
  * @version 1.0
  */
 
@@ -66,7 +68,45 @@ class NotizFXImplTest {
     @Test
      void setFaelligkeit() {
        NotizFXImpl notizFXImpl = new NotizFXImpl();
-       assertThrows(ObjectIstNullException.class,  () -> {notizFXImpl.setFaelligkeit(objectIsNull);}); }
+       assertThrows(ObjectIstNullException.class,  () -> {notizFXImpl.setFaelligkeit(objectIsNull);});
+    }
+
+    /**
+     * Testet, was bei Übergabe einer NotizFX mit gleicher ID passiert
+     * Methode: equals()
+     * ->Erwartet wird der Rückgabewert true
+     */
+    @Test
+    void equalsNotizenSindGleich(){
+        NotizFX notizFX1 = new NotizFXImpl();
+        notizFX1.setNotizID(244);
+
+        NotizFX notizFX2 = new NotizFXImpl();
+        notizFX2.setNotizID(244);
+
+        boolean ergebnis = notizFX1.equals(notizFX2);
+
+        assertEquals(true, ergebnis);
+    }
+
+    /**
+     * Testet, was bei Übergabe einer KategorieFX mit ungleicher ID passiert
+     * Methode: equals()
+     * ->Erwartet wird der Rückgabewert false
+     */
+    @Test
+    void equalsNotizenSindUngleich(){
+        NotizFX notizFX1 = new NotizFXImpl();
+        notizFX1.setNotizID(244);
+
+        NotizFX notizFX2 = new NotizFXImpl();
+        notizFX2.setNotizID(249);
+
+        boolean ergebnis = notizFX1.equals(notizFX2);
+
+        assertEquals(false, ergebnis);
+    }
+
 
 //TODO J Unit Methoden auskommentieren, falls in Klasse NotizFXImpl implementiert.
   //  @Test
