29d4fcfd7a700429366e2e525a4f2ba48924b8a7
TG <orggottschalk@web.de>
2018-09-30 19:09:20 +0200

    Kommentare und JUNIT Test hinzugefuegt.

--- src/main/Java/notizverwaltung/model/interfaces/Notiz.java
+++ src/main/Java/notizverwaltung/model/interfaces/Notiz.java
@@ -20,32 +20,41 @@ public interface Notiz
 {
 
 
+    //____________________ID____________________
     public int getNotizID();
     public void setID(int notizID);
 
+    //____________________TITLE____________________
     public String getTitle();
     public void setTitle(String title) throws StringIsEmptyException;
 
+    //____________________KATEGORIE____________________
     public int getKategorieID();
-    public void setKategorieID(int kategorieID);
+   public void setKategorieID(int kategorieID);
 
+    //____________________BEARBEITUNGSZUSTAND____________________
     public int getBearbeitungszustandID();
     public void setBearbeitungszustandID(int bearbeitungszustandID);
 
 
+    //____________________BESCHREIBUNG____________________
     public String getBeschreibung();
     public void setBeschreibung(String beschreibung) throws StringIsEmptyException;
 
+    //____________________PRIORITÄT____________________
     public boolean getPrioritaet();
     public void setPrioritaet(boolean prioritaet);
 
+    //____________________FÄLLIGKEIT____________________
     public Date getFaelligkeit();
     public void setFaelligkeit(Date faelligkeit) throws ObjectIstNullException;
 
+    //____________________ERRINERUNG____________________
     public boolean istErinnerungGesetzt();
     public Date getErinnerung();
     public void setErinnerung(Date erinnerung) throws ObjectIstNullException;
 
+    //____________________ERSTELLUNG____________________
     public Date getErstellung();
 
 
